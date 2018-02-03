65a6ba5c53ff737ff59b018ea2b4d22288733b31
Valentin DUMONT <valentin.dumont03@outlook.fr>
2018-02-03 16:17:16 +0100

    Disable bar title in WelcomeActivity and added do you know textview

--- TACA/app/src/main/java/com/zzcoin/taca/WelcomeActivity.java
+++ TACA/app/src/main/java/com/zzcoin/taca/WelcomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -11,7 +12,9 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_welcome);
+
 
         Button questionButton = (Button) findViewById(R.id.QuestionButton);
         questionButton.setOnClickListener(new View.OnClickListener() {
